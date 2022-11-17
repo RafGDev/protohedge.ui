86d2f7bc5d62c33d03afaf1a3ac44b884867e31f
Rafael Goesmann <rgo51@uclive.ac.nz>
2022-11-17 22:43:43 +1100

    Comment out unneeded code

--- components/position-pie-chart/position-pie-chart.tsx
+++ components/position-pie-chart/position-pie-chart.tsx
@@ -2,25 +2,25 @@ import BigNumber from "bignumber.js";
 import { Pie, PieChart } from "recharts";
 
 export function PositionPieChart() {
-  const { data, error, isLoading } = useVault();
+  // const { data, error, isLoading } = useVault();
 
-  if (isLoading) return <>Loading..</>;
+  // if (isLoading) return <>Loading..</>;
   
-  console.log('data is: ');
-  console.log(data);
+  // console.log('data is: ');
+  // console.log(data);
 
-  const data03 = data?.map(d => ({
-    name: d.symbol,
-    value: d.amount.dividedBy(10**d.decimals).toNumber(),
-  }));
+  // const data03 = data?.map(d => ({
+  //   name: d.symbol,
+  //   value: d.amount.dividedBy(10**d.decimals).toNumber(),
+  // }));
 
-  console.log(data03);
+  // console.log(data03);
 
-  return (
-    <PieChart width={730} height={250}>
-      <Pie data={data03} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#82ca9d" label={position => {
-        return `${position.name} (${new Number(position.percent * 100).toFixed(0)})%`
-      }} />
-    </PieChart>
-  );
+  // return (
+  //   <PieChart width={730} height={250}>
+  //     <Pie data={data03} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#82ca9d" label={position => {
+  //       return `${position.name} (${new Number(position.percent * 100).toFixed(0)})%`
+  //     }} />
+  //   </PieChart>
+  // );
 }

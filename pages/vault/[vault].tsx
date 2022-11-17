e2d05046af3570f195cd0754486a97555249e0c0
Rafael Goesmann <rgo51@uclive.ac.nz>
2022-11-17 22:58:57 +1100

    fix build error

--- pages/vault/[vault].tsx
+++ pages/vault/[vault].tsx
@@ -2,8 +2,6 @@ import { useRouter } from 'next/router';
 import { ExposureChart } from '../../components/exposure-chart/exposure-chart';
 import { useVault } from '../../hooks/use-vault';
 
-
-
 export default function VaultContainer() {
   const router = useRouter();
   if (!router.isReady) return '...Loading';

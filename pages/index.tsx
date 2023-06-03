aebf3e12e0dfa6b45384a836eae5a5323dca41a9
Rafael Goesmann <rafaelgoesmann@gmail.com>
2023-06-03 14:04:46 +1000

    Copy vault.tsx to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,98 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { ExposureChart } from "../components/exposure-chart/exposure-chart";
+import { LiquidityIndicator } from "../components/liquidity-indicator/liquidity-indicator";
+import { PnlChart } from "../components/pnl-chart/pnl-chart";
+import { PositionsChart } from "../components/positions-chart/positions-chart";
+import { RebalanceNotes } from "../components/rebalance-notes/rebalance-notes";
+import { RebalanceTimer } from "../components/rebalance-timer/rebalance-timer";
+import Stats from "../components/stats/stats";
+import { useHistoricPnl } from "../hooks/use-historic-pnl";
+import { useRebalanceNotes } from "../hooks/use-rebalance-history";
+import { useRebalanceInfo } from "../hooks/use-rebalance-info";
+import { useVault } from "../hooks/use-vault";
 
-const Home: NextPage = () => {
+export default function VaultContainer() {
+  const router = useRouter();
+  if (!router.isReady) return "...Loading";
+  const { vault } = router.query;
+
+  if (!vault) {
+    return router.back();
+  }
+
+  return <Vault vaultAddress={vault as string} />;
+}
+
+interface VaultProps {
+  vaultAddress: string;
+}
+
+const TopGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+  padding: 10px;
+  min-height: 100px;
+`;
+
+function Vault(props: VaultProps) {
+  const {
+    data: vault,
+    error: vaultError,
+    isLoading: vaultIsLoading,
+  } = useVault(props.vaultAddress);
+  const {
+    data: historicPnl,
+    error: historicPnlError,
+    isLoading: historicPnlIsLoading,
+  } = useHistoricPnl(props.vaultAddress);
+  const {
+    data: rebalanceNotes,
+    error: rebalanceNotesError,
+    isLoading: rebalanceNotesIsLoading,
+  } = useRebalanceNotes(props.vaultAddress);
+  const {
+    data: rebalanceInfo,
+    error: rebalanceError,
+    isLoading: rebalanceInfoIsLoading,
+  } = useRebalanceInfo(props.vaultAddress);
+
+  if (
+    vaultIsLoading ||
+    !vault ||
+    historicPnlIsLoading ||
+    !historicPnl ||
+    rebalanceNotesIsLoading ||
+    !rebalanceNotes ||
+    rebalanceInfoIsLoading ||
+    !rebalanceInfo
+  )
+    return <>'...Loading'</>;
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <TopGrid>
+        <Stats vault={vault} />
+        <RebalanceTimer rebalanceInfo={rebalanceInfo} />
+      </TopGrid>
+      <LiquidityIndicator vault={vault} />
 
+      <div className="grid grid-cols-2 gap-8">
+        <div className="min-h-[250px]">
+          <ExposureChart vault={vault} />
+        </div>
+        <div className="min-h-[250px]">
+          <PositionsChart vault={vault} />
+        </div>
+        <div className="min-h-[250px]">
+          <PnlChart historicPnl={historicPnl} />
+        </div>
+        <div className="min-h-[250px]">
+          <RebalanceNotes rebalanceNotes={rebalanceNotes} />
+        </div>
+        <div className="min-h-[250px]"></div>
+      </div>
     </div>
   );
 }
-export default Home
